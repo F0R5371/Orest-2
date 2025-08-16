92e994766280d8c6e52d7381bd88200d420d99a0
4LEX <106479376+F0R5371@users.noreply.github.com>
2025-08-15 19:38:58 -0500

    Init orest.py

--- orest.py
+++ orest.py
@@ -8,19 +8,39 @@ class Orest():
         
     def setup(self):
         
+        # Root setup
         root = CTk()
         root.title("Orest")
-        root.geometry("1500x900")
+        root.geometry("1000x500")
+
+        # Sidebar frame ----------------------------------------------
+        sidebar = CTkFrame(root)
+        sidebar.pack(side = 'left', fill = 'y')
+
+        sidebar_contents = CTkFrame(sidebar)
+        sidebar_contents.pack(padx = (10, 10), pady = (10, 10), fill = 'both', expand = True)
+
+        toggle_button = CTkButton(sidebar_contents, text = "Toggle")
+        toggle_button.pack(side = 'top')
+
+        add_button = CTkButton(sidebar_contents, text = "Add")
+        add_button.pack(side = 'bottom')
+
+        # Main frame ----------------------------------------------
+        main = CTkFrame(root)
+        main.pack(padx = (30, 30), pady = (30, 30), fill = 'both', expand = True)
+
+        main.grid_columnconfigure(0, weight = 1)
         
         # Response box
-        response = CTkTextbox(root)
-        response.pack(padx = (30, 30), pady = (30, 30), fill = 'both')
+        response = CTkTextbox(main, state = "disabled")
+        response.grid(row = 0, sticky = "NSEW", pady = (0, 30))
         
         # Entry box
-        entry = CTkFrame(root)
-        entry.pack(padx = (30, 30), pady = (30, 30), fill = 'both')
+        entry = CTkFrame(main)
+        entry.grid(row = 1, sticky = "NSEW")
         
-        entry_box = CTkEntry(entry)
+        entry_box = CTkEntry(entry, height = 50, font = ('Times New Roman', 24))
         entry_box.grid(row = 0, column = 0, sticky = "NSEW", padx = (0, 10))
         
         entry_button = CTkButton(entry, text = "Enter")
@@ -28,6 +48,8 @@ class Orest():
         
         entry.grid_columnconfigure(0, weight = 15)
         entry.grid_columnconfigure(1, weight = 1)
+
+        # Mainloop ----------------------------------------------
 
         root.mainloop()
         
